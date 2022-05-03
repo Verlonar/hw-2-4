f8e105f7835de6ffac733c8ab793ad6650eaa16f
Verlonar <garvlok@gmail.com>
2022-05-03 14:33:43 +0300

    Initial commit

---
+++ src/test/java/ru/verlonar/hw24/Hw24ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.verlonar.hw24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
